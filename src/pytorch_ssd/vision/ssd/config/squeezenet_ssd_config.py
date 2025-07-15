073e04168ded346b2a67b384d6e6ef5da67fde5b
Your Duvan Marrugo <duvan.andres@cross-compass.com>
2025-07-15 10:55:12 +0900

    Pytorch-ssd: Fix lib paths.

--- src/pytorch_ssd/vision/ssd/config/squeezenet_ssd_config.py
+++ src/pytorch_ssd/vision/ssd/config/squeezenet_ssd_config.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from vision.utils.box_utils import SSDSpec, SSDBoxSizes, generate_ssd_priors
+from pytorch_ssd.vision.utils.box_utils import SSDSpec, SSDBoxSizes, generate_ssd_priors
 
 
 image_size = 300
